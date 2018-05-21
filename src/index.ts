e050566b7cb23b681c5901eb632a5beaa90c7c4c
Victor Ribero <victor.ribero3@gmail.com>
2018-05-21 11:20:54 +0200

    Added form to add tabs dynamically
    * We can add tabs dynamically
    
    * We can choose the text of the tab and its content
    
    * We can cancel the <form> to create a tab

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,42 @@ import './styles/main.sass';
 import Accordion from "./Infrastructure/Accordion/Entity/Accordion";
 
 const accordion = new Accordion(document.getElementById('example1'));
-accordion.addTab('newtab', 'newcontent');
+
+
+const example1 = document.getElementById('example1-wrapper');
+const formOpeners = example1.getElementsByClassName('Button-openForm');
+
+// Open the FORM
+for (let i = 0, n = formOpeners.length; i < n; i++) {
+  formOpeners[i].addEventListener('click', event => {
+    const form = example1.getElementsByClassName('AddSectionForm')[0];
+    form.classList.remove('u-none');
+  });
+}
+
+// Submit the FORM
+const form = example1.getElementsByClassName('AddSectionForm')[0];
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const tabText = form.getElementsByClassName(
+    'AddSectionForm-input--tab'
+  )[0].value;
+  const tabContextText = form.getElementsByClassName(
+    'AddSectionForm-input--tabcontext'
+  )[0].value;
+
+  accordion.addTab(tabText, tabContextText);
+  form.classList.add('u-none');
+
+});
+
+// Cancel the FORM
+const cancelFormButtons = example1.getElementsByClassName(
+  'AddSectionForm-actionBarButton--cancel'
+);
+for (let i = 0, n = formOpeners.length; i < n; i++) {
+  cancelFormButtons[i].addEventListener('click', event => {
+    const form = example1.getElementsByClassName('AddSectionForm')[0];
+    form.classList.add('u-none');
+  });
+}
